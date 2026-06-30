b8820c8861d951e959d4b462be54f5edb3ce09b7
axiom-S25u <axioms25u@gmail.com>
2026-06-30 16:25:21 +0300

    uhh fix

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,12 @@
 #include <Geode/utils/web.hpp>
 #include <Geode/utils/async.hpp>
 #include <Geode/utils/coro.hpp>
+#include <Geode/utils/base64.hpp>
 #include <string>
 #include <vector>
 #include <cstdlib>
 #include <algorithm>
+#include <map>
 
 using namespace geode::prelude;
 
@@ -19,6 +21,61 @@ struct Info {
 };
 
 static std::vector<Info> pool;
+// this is NOT the same thing as loadingscreentweaks, lst has custom ones YOU choose, this..well.. doesnt, its all random lol
+std::vector<std::string> split(const std::string& str, char delim)
+{
+	std::vector<std::string> res;
+	std::string temp;
+	for (char c : str)
+	{
+		if (c==delim)
+		{
+			res.push_back(temp);
+			temp.clear();
+		}
+		else
+		{
+			temp+=c;
+		}
+	}
+	if (!temp.empty())
+	{
+		res.push_back(temp);
+	}
+	return res;
+}
+
+std::map<std::string, std::string> parseRobTop(const std::string& str, char splitter)
+{
+	std::map<std::string, std::string> res;
+	std::vector<std::string> parts=split(str, splitter);
+	for (size_t n=0; n+1<parts.size(); n+=2)
+	{
+		res[parts[n]]=parts[n+1];
+	}
+	return res;
+}
+
+std::string decBase64(std::string_view src)
+{
+	std::string s(src);
+	for (auto& c : s)
+	{
+		if (c=='-') c='+';
+		if (c=='_') c='/';
+	}
+	auto dec=geode::utils::base64::decodeString(s, geode::utils::base64::Base64Variant::Normal);
+	if (dec.ok())
+	{
+		return dec.unwrap();
+	}
+	auto dec2=geode::utils::base64::decodeString(s, geode::utils::base64::Base64Variant::Url);
+	if (dec2.ok())
+	{
+		return dec2.unwrap();
+	}
+	return std::string(src);
+}
 
 arc::Future<void> getWisdom()
 {
@@ -26,46 +83,74 @@ arc::Future<void> getWisdom()
 	{
 		int pg=std::rand()%351;
 		web::WebRequest req;
-		web::WebResponse res=co_await req.get("https://gdbrowser.com/api/search/*?page="+std::to_string(pg)+"&type=mostdownloaded");
+		req.header("Content-Type", "application/x-www-form-urlencoded");
+		req.userAgent("");
+		req.bodyString("gameVersion=22&binaryVersion=47&gdw=0&type=1&page="+std::to_string(pg)+"&secret=Wmfd2893gb7");//boom,lings?
+		web::WebResponse res=co_await req.post("http://www.boomlings.com/database/getGJLevels21.php"); 
 		if (!res.ok()||res.code()==204)
 		{
 			continue;
 		}
-		auto jsonRes=res.json();
-		if (!jsonRes.ok()||!jsonRes.unwrap().isArray()||jsonRes.unwrap().size()==0)
+		std::string body=res.string().unwrapOr("");
+		if (body.empty()||body=="-1"||body.starts_with("error"))
 		{
 			continue;
 		}
-		matjson::Value lvl=jsonRes.unwrap()[std::rand()%jsonRes.unwrap().size()];
-		geode::Result<std::string> idRes=lvl["id"].asString();
-		if (!idRes.ok())
+		std::vector<std::string> parts=split(body, '#');
+		if (parts.empty())
 		{
 			continue;
 		}
-		std::string idStr=idRes.unwrap();
+		std::vector<std::string> lvls=split(parts[0], '|');
+		if (lvls.empty())
+		{
+			continue;
+		}
+		std::string levelStr=lvls[std::rand()%lvls.size()];
+		std::map<std::string, std::string> lvl=parseRobTop(levelStr, ':');
+		std::string idStr=lvl["1"];
+		if (idStr.empty())
+		{
+			continue;
+		}
+		
 		int cpg=std::rand()%5;
-		res=co_await req.get("https://gdbrowser.com/api/comments/"+idStr+"?page="+std::to_string(cpg));
+		web::WebRequest req2;
+		req2.header("Content-Type", "application/x-www-form-urlencoded");
+		req2.userAgent("");
+		req2.bodyString("gameVersion=22&binaryVersion=47&gdw=0&levelID="+idStr+"&page="+std::to_string(cpg)+"&count=10&mode=0&secret=Wmfd2893gb7");
+		res=co_await req2.post("http://www.boomlings.com/database/getGJComments21.php");
 		if (!res.ok()||res.code()==204)
 		{
 			continue;
 		}
-		auto cjsonRes=res.json();
-		if (!cjsonRes.ok()||!cjsonRes.unwrap().isArray()||cjsonRes.unwrap().size()==0)
+		std::string cbody=res.string().unwrapOr("");
+		if (cbody.empty()||cbody=="-1"||cbody.starts_with("error"))
 		{
 			continue;
 		}
-		matjson::Value root=cjsonRes.unwrap();
-		for (int n=0; n<(int)root.size(); n++)
+		std::vector<std::string> cparts=split(cbody, '#');
+		if (cparts.empty())
 		{
-			matjson::Value item=root[n];
-			geode::Result<std::string> contentRes=item["content"].asString();
-			geode::Result<std::string> userRes=item["username"].asString();
-			if (contentRes.ok()&&userRes.ok())
+			continue;
+		}
+		std::vector<std::string> comments=split(cparts[0], '|');
+		for (int n=0; n<(int)comments.size(); n++)
+		{
+			std::vector<std::string> cinfo=split(comments[n], ':');
+			if (cinfo.size()>=2)
 			{
-				Info inf;
-				inf.usr=userRes.unwrap();
-				inf.txt=contentRes.unwrap();
-				pool.push_back(inf);
+				std::map<std::string, std::string> commentData=parseRobTop(cinfo[0], '~');
+				std::map<std::string, std::string> userData=parseRobTop(cinfo[1], '~');
+				std::string rawTxt=commentData["2"];
+				std::string username=userData["1"];
+				if (!rawTxt.empty()&&!username.empty())
+				{
+					Info inf;
+					inf.usr=username;
+					inf.txt=decBase64(rawTxt);
+					pool.push_back(inf);
+				}
 			}
 		}
 		if (!pool.empty())
@@ -118,7 +203,7 @@ class $modify(MyLoadingLayer, LoadingLayer) {
 			CCLabelBMFont* line=CCLabelBMFont::create(str.c_str(), "goldFont.fnt");
 			line->setPosition(textArea->getPosition());
 			line->limitLabelWidth(420.f, textArea->getScale(), .25f);
-			line->setID("words-of-wisdom"_spr);
+			line->setID("words-of-wisdom"_spr); // yet again, its not the same like loadingscreentweaks
 			this->addChild(line);
 		}
 		s_holder.spawn(getWisdom(), []() {});
